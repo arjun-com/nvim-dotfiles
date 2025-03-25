e2c5f3a4a1a435044c1fadfc7fcbb3bd5a0e6db0
Arjun <rao.d.arjun@gmail.com>
2025-03-25 15:29:03 +0530

    idk

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -13,18 +13,25 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
+		"nvim-lua/plenary.nvim"
+	},
+	{
 		"nvim-tree/nvim-tree.lua",
 		requires = {
 			"nvim-tree/nvim-web-devicons"
 		},
 		config = function()
-			require("nvim-tree").setup{}
+			require("nvim-tree").setup {}
 		end
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
 		config = function()
-			require("nvim-treesitter").setup{}
+			require("nvim-treesitter").setup {
+				ensure_installed = { "javascript", "typescript", "tsx" },
+				highlight = { enable = true },
+				context_commentstring = { enable = true }
+			}
 		end
 	},
 	{
@@ -34,7 +41,7 @@ require("lazy").setup({
 		},
 		config = function()
 			vim.opt.termguicolors = true -- Also declared in plugins.options cuz this technically is suppossed to go there.
-			require("bufferline").setup{}
+			require("bufferline").setup {}
 		end
 	},
 	{
@@ -42,23 +49,28 @@ require("lazy").setup({
 		tag = "0.1.5",
 		version = "0.1.5",
 		dependencies = { "nvim-lua/plenary.nvim" }
-    },
+	},
 	{
 		"terrortylor/nvim-comment",
+		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
 		config = function()
-			require("nvim_comment").setup{ create_mappings = false }
+			require("nvim_comment").setup { create_mappings = false,
+				hook = function()
+					require("ts_context_commentstring.internal").update_commentstring()
+				end
+			}
 		end
 	},
 	{
 		"williamboman/mason.nvim",
 		config = function()
-			require("mason").setup{}
+			require("mason").setup {}
 		end
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
-			require("mason-lspconfig").setup{
+			require("mason-lspconfig").setup {
 				ensure_installed = { "lua_ls", "tailwindcss", "tsserver", "gopls", "rust_analyzer", "pyright", "html" }
 			}
 		end
@@ -67,42 +79,63 @@ require("lazy").setup({
 		"neovim/nvim-lspconfig"
 	},
 	{
-    	"hrsh7th/nvim-cmp",
-    	event = "InsertEnter",
-    	dependencies = {
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
 			{
-        		"L3MON4D3/LuaSnip",
-        		dependencies = "rafamadriz/friendly-snippets",
-        		opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-        		config = function(_, opts)
-          			require("luasnip").config.set_config{opts}
-          			-- require "configs.luasnip"
-        		end,
+				"L3MON4D3/LuaSnip",
+				dependencies = "rafamadriz/friendly-snippets",
+				opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+				config = function(_, opts)
+					require("luasnip").config.set_config { opts }
+					-- require "configs.luasnip"
+				end,
 			}
 		}
-    },
-    {
+	},
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					javascript = { "prettier" },
+					javascriptreact = { "prettier" },
+					css = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					yaml = { "prettier" },
+					python = { "isort", "black" }
+				},
+				format_on_save = {
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 750
+				}
+			})
+		end
+	},
+	{
 		"windwp/nvim-autopairs",
 		opts = {
 			fast_wrap = {},
 			disable_filetype = { "TelescopePrompt", "vim" },
 		},
 		config = function(_, opts)
-			require("nvim-autopairs").setup{opts}
+			require("nvim-autopairs").setup { opts }
 		end
-    },
-    {
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-    },
+	},
+	{
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+	},
 	{
 		"EdenEast/nightfox.nvim",
 		name = "carbonfox",
 		config = function()
-			require("nightfox").setup{
+			require("nightfox").setup {
 				options = {
 					transparent = true
 				}
